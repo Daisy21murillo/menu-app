a48f60ba78ba5d9abc402dd091fb63ccd28b68d3
Daisy Murillo <daisy21murillo@gmail.com>
2020-07-10 22:28:01 -0700

    Finished basic menu app for Grocery Items. It can show items, add items, delete items, and delete all items

--- menu-exercise/src/GroceriesMenuApp.java
+++ menu-exercise/src/GroceriesMenuApp.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class GroceriesMenuApp {
 
 	static Scanner input = new Scanner(System.in);
+	static int addingIndex = 0;
 	
 	public static void main(String[] args) {
 		
@@ -34,13 +35,13 @@ public class GroceriesMenuApp {
 				showMenuItem(menu);
 				
 			} else if (decision == 3) {
-				//addMenuItem(menu);
+				addMenuItem(menu);
 				
 			} else if (decision == 4) {
-				//deleteMenuItem(menu);
+				deleteMenuItem(menu);
 				
 			} else {
-				//deleteAllMenuItems(menu);
+				deleteAllMenuItems(menu);
 			}
 		}
 
@@ -60,10 +61,47 @@ public class GroceriesMenuApp {
 	
 	public static void showMenuItem(String[] menu) {
 		System.out.println("Which Grocery Item would you like to see?");
-		
 		int menuNum = input.nextInt();
-		System.out.println(menuNum + ": " + menu[menuNum]);
 		
+		if (isValid(menuNum)) {
+			System.out.println(menuNum + ": " + menu[menuNum]);
+		}
+	
+	}
+	
+	public static void addMenuItem(String[] menu) {
+		System.out.println("Enter new Grocery Item: ");
+		String newMember = input.next();
+		
+		if (isValid(addingIndex)) {
+			menu[addingIndex++] = newMember;
+		} 
+		
+	}
+	
+	public static void deleteMenuItem(String[] menu) {
+		System.out.println("What item would you like to delete?");
+		int deleteMember = input.nextInt();
+		
+		if(isValid(deleteMember)) {
+			menu[deleteMember] = null;
+		}
+	}
+	
+	public static void deleteAllMenuItems(String[] menu) {
+		for(int i = 0; i < menu.length; i++) {
+			menu[i] = null;
+		}
+	}
+	
+//	checks the validity of the user input for menu item numbers
+	public static boolean isValid(int choice) {
+		if(choice >= 0 && choice < 5) {
+			return true;
+		} else {
+			System.out.println("Invalid Entry.");
+			return false;
+		}
 	}
 
 	public static void showMenu() {
